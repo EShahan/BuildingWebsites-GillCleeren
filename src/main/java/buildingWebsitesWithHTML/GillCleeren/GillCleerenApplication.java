70ca8bc4af1a77c4d9261318735d911e3890c628
Eric Shahan <ctibalt@gmail.com>
2024-06-03 17:26:52 -0500

    first commit

---
+++ src/main/java/buildingWebsitesWithHTML/GillCleeren/GillCleerenApplication.java
@@ -0,0 +1,13 @@
+package buildingWebsitesWithHTML.GillCleeren;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GillCleerenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GillCleerenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/buildingWebsitesWithHTML/GillCleeren/GillCleerenApplicationTests.java
@@ -0,0 +1,13 @@
+package buildingWebsitesWithHTML.GillCleeren;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GillCleerenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
